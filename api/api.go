e89d57a8040bc9ecee14a9dc5f6386ffdfbcd969
24thsaint <rave.arevalo@kingsland.io>
2019-08-14 12:01:03 +0800

    fix(api): Add PATCH method to CORS response configuration

--- api/api.go
+++ api/api.go
@@ -31,7 +31,7 @@ type APIListResult struct {
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, PATCH, PUT, DELETE, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	(*w).Header().Set("Content-Type", "application/json")
 }
